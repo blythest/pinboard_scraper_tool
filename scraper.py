75efeac77d1eb00175634d458bac15cbe6280adb
Blythe Sheldon <bsheldon@ulive.com>
2015-03-02 20:57:25 -0800

    Init commit. Pull article info from pinboard links.

--- scraper.py
+++ scraper.py
@@ -7,15 +7,16 @@ from newspaper import Config, Article, Source
 import smtplib
 
 PB_USERNAME = "******"
-PB_PASSWORD = "******"
+PB_PASSWORD = "*****"
 PB_URL = "https://api.pinboard.in/v1/posts/all?format=json"
 
-class Pinboard_Connection:
+class Connection:
   def __init__(self):
     self.data = requests.get(PB_URL, auth=(PB_USERNAME, PB_PASSWORD))
     self.links = self.create_pinboard_links()
+    self.today = self.pintime_today()
 
-  def pintime_today():
+  def pintime_today(self):
     today = datetime.date.today()
     format = "%Y-%m-%d"
     return today.strftime(format)
@@ -23,10 +24,25 @@ class Pinboard_Connection:
   def create_pinboard_links(self):
     pinboard_json = self.convert_pins_to_json()
     for pin in pinboard_json:
-      if '2015-02' in pin['time']:
-        yield pin['href']
+      if self.pintime_today() in pin['time']:
+        yield(pin['href'])
 
   def convert_pins_to_json(self):
     pin_articles = ''
     pinboard_json = json.loads(self.data.text)
     return pinboard_json
+
+class Articles_Info(Connection):
+
+  def articles(self):
+    for link in self.links:
+      pin_article = Article(link)
+      pin_article.download()
+      pin_article.parse()
+      yield(pin_article.html)
+
+
+
+pinboard_articles = Articles_Info()
+for p in pinboard_articles.articles():
+  print(p)
